439bc6122d2b49c24b0bbbb48202630489f0ac16
Raymond Lowe <raymond@wlmedia.com>
2022-02-15 21:45:40 +0800

    Draft not working

--- find-heavy-assets.py
+++ find-heavy-assets.py
@@ -26,6 +26,7 @@ def read_apache_log_as_dataframe(file_name):
 
     # Create a blank dataframe
     df = pd.DataFrame()
+    
 
 
     i = 0
@@ -40,11 +41,36 @@ def read_apache_log_as_dataframe(file_name):
         # split line on spaces but consider quoted strings as one word
         line_list = [p for p in re.split("( |\\\".*?\\\"|'.*?')", line) if p.strip()]      
 
+        # split line_list[4] on spaces and insert into list
+        request_list = [p for p in re.split("( |\\\".*?\\\"|'.*?')", line_list[4]) if p.strip()]
+
+        # insert request_list into line_list in the 4th place
+        # line_list.insert(4, request_list)
+
+
         # append line_list as a new row at the end of the dataframe df
         df = df.append(pd.Series(line_list), ignore_index=True)
 
     # Close the input file
     file.close()
+
+    # rename the df columns to ip, dash, dash2, datetime, request, status, bytes, dash3, user_agent
+    df.columns = ['ip', 'dash', 'dash2', 'datetime', 'request', 'status', 'bytes', 'dash3', 'user_agent', 'dash4', 'dash5']
+
+    # remove quotes from requests column
+    df['request'] = df['request'].str.replace('"', '')
+
+    # split column request into three columns on space
+    df[['request_type', 'request_path', 'request_query']] = df['request'].str.split(' ', 2, expand=True)
+
+    # delete columns, dash, dash2 and dash3, request, user_agent
+    df = df.drop(['dash', 'dash2', 'dash3', 'request','user_agent'], axis=1)
+
+    # remove double quote from datetime column
+    df['datetime'] = df['datetime'].str.replace('"', '')
+
+    # parse apache log date time with timezone into datetime format
+    df['datetime'] = pd.to_datetime(df['datetime'], format='%d/%b/%Y:%H:%M:%S %z')
 
     # return the dataframe
 
@@ -59,6 +85,10 @@ def read_apache_log_as_dataframe(file_name):
 ## main so that this file can be imported as a module
 if __name__ == '__main__':
     # call the apache_to_csv function using the first arg as the parameter
-    print(read_apache_log_as_dataframe(sys.argv[1]))
+    apache_log_df = read_apache_log_as_dataframe(sys.argv[1])
 
-    
+    report =pd.pivot_table(apache_log_df,index=['request_type', 'request_path'], values='bytes', aggfunc=['sum','mean','count'])
+
+
+    print(apache_log_df)
+    print(report) 
